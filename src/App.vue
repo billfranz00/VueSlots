20ba6a86e17ded31be870a7667f11ccbdd219235
billyf <Maggie91>
2018-09-28 16:50:54 -0700

    First commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+            	<!-- <app-quote quote="A wonderful quote!"></app-quote> -->
+            	<app-quote>
+            		<h2 slot="title">{{ quoteTitle }}</h2>
+            		<!-- <p slot="content">A wonderful Quote</p> -->
+            		<p>A wonderful Quote</p>
+            	</app-quote>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+	import Quote from './components/Quote.vue';
+
+    export default {
+    	data: function() {
+    		return {
+    			quoteTitle: 'The Quote'
+    		}
+    	},
+        components: {
+        	appQuote: Quote
+        }
+    }
+</script>
+
+<style>
+	h2 {
+		color: green;
+	}
+</style>
---
+++ src/components/Quote.vue
@@ -0,0 +1,41 @@
+<template>
+	<div>
+		<!-- <p>{{ quote }}</p> -->
+		<div class="title">
+			<slot name="title"></slot>
+			<span><slot name="subtitle">The Subtitle</slot></span>
+		</div>
+		<div>
+			<!-- <slot name="content"></slot> -->
+			<slot></slot>
+		</div>
+	</div>
+</template>
+
+<script type="text/javascript">
+	export default {
+		// props: ['quote']
+	}
+</script>
+
+<style type="text/css" scoped>
+	div {
+		border: 1px solid #ccc;
+		box-shadow: 1px 1px 2px black;
+		padding: 30px;
+		margin: 30px auto;
+		text-align: center;
+	}
+
+	h2 {
+		color: red;
+	}
+
+	.title {
+		font-style: italic;
+	}
+
+	span {
+		color: #ccc;
+	}
+</style>
